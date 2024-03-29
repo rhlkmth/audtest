3946829822e9455193b638fdfdece790ab22883f
rhlkmth <rahulkamath272@gmail.com>
2024-03-29 20:40:47 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,18 @@
 import '@/styles/globals.css'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import NextApp from "next/app";
 
-const theme = extendTheme({});
+class App extends NextApp {
+  render() {
+    const { Component, pageProps } = this.props;
 
-
-export default function App({ Component, pageProps }) {
-  return (
-    <ChakraProvider theme={theme}> {/* If using a custom theme */}
-      <Component  {...pageProps} />
-    </ChakraProvider>
-  );
+    return (
+      <ChakraProvider>
+        <ColorModeScript initialColorMode="light" />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    );
+  }
 }
+
+export default App;
